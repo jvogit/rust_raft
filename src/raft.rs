570d5e02016cde237bca2e982ef485ee18b79387
jvogit <justinxvopro@gmail.com>
2023-09-08 12:41:29 -0700

    thread loop

--- src/raft.rs
+++ src/raft.rs
@@ -1,3 +1,7 @@
+use std::{sync::mpsc::{Receiver, self}, thread::{JoinHandle, self}};
+
+use crate::rpc::{RPCConfig, RPC};
+
 enum ServerState {
     Follower,
     Candidate,
@@ -5,7 +9,11 @@ enum ServerState {
 }
 
 pub struct Server {
+    // Server metadata
+    id: usize,
+    config: RPCConfig,
     state: ServerState,
+    // RAFT variables
     // Stable
     currentTerm: usize,
     votedFor: usize,
@@ -19,5 +27,38 @@ pub struct Server {
 }
 
 impl Server {
-    
+    pub fn new(id: usize, config: RPCConfig) -> Server {
+        Server {
+            id,
+            config,
+            state: ServerState::Follower,
+            currentTerm: 0,
+            votedFor: 0,
+            log: vec![],
+            commitIndex: 0,
+            lastApplied: 0,
+            nextIndex: vec![],
+            matchIndex: vec![],
+        }
+    }
+
+    pub fn start_thread(self, recv: Receiver<RPC>) -> JoinHandle<()> {
+        thread::spawn(move || {
+            loop {
+                match recv.recv_timeout(self.config.election_timeout) {
+                    Ok(rpc) => self.handle(rpc),
+                    Err(mpsc::RecvTimeoutError::Timeout) => self.handle_timeout(),
+                    Err(_) => break,
+                }
+            }
+        })
+    }
+
+    fn handle(&self, rpc: RPC) {
+
+    }
+
+    fn handle_timeout(&self) {
+
+    }
 }
--- src/rpc.rs
+++ src/rpc.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap, sync::mpsc, time::Duration};
+
 pub struct AppendEntries {
     term: usize,
     leaderId: usize,
@@ -13,10 +15,20 @@ pub struct RequestVote {
     lastLogTerm: usize,
 }
 
-
 pub enum RPC {
     AppendEntries(AppendEntries),
     AppendEntriesRes(usize, bool),
     RequestVote(RequestVote),
-    RequestVoteRes(usize, bool)
+    RequestVoteRes(usize, bool),
+}
+
+pub struct RPCConfig {
+    connections: HashMap<usize, mpsc::Sender<RPC>>,
+    pub election_timeout: Duration,
+}
+
+impl RPCConfig {
+    pub fn get_connection(&self, id: usize) -> Option<&mpsc::Sender<RPC>> {
+        self.connections.get(&id)
+    }
 }
